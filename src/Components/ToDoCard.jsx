4fad697cf07c4ea10825556f93b36416a4f74da0
CadeOLink <oliveira.caldeira@outlook.com>
2024-09-05 11:11:04 -0300

    :construction: Armazenando em LocalStorage

--- src/Components/ToDoCard.jsx
+++ src/Components/ToDoCard.jsx
@@ -1,13 +1,16 @@
 import { Container, Row, Col, Image, Button} from "react-bootstrap"
 
 function ToDoCard(){
+
+   let NewToDo = JSON.parse(localStorage.getItem("NewToDo"))
+
    return(
       <Row>
          <Col md={5} style={{padding:"0px"}}>
-            Titulo
+            {NewToDo != null ? NewToDo.Title : ""}
          </Col>
          <Col md={5} style={{padding:"0px"}}>
-            Descrição
+            {NewToDo != null ? NewToDo.Description : ""}
          </Col>
          <Col md={1} style={{padding:"0px"}}>
          <Button></Button>
--- src/Routers/CreateNewToDo.jsx
+++ src/Routers/CreateNewToDo.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react"
 import { Col, Button, Row, Container } from "react-bootstrap"
 import { Form } from "react-bootstrap"
 
 function CreateNewToDo(){
+
+   const [Description, setDescription] = useState('');
+   const [Title, setTitle] = useState('');
+   
+   function getDescription(e){
+      setDescription(e.target.value) 
+   }
+   function getTitle(e){
+      setTitle(e.target.value) 
+   }
+
+   function createNewToDo(Description, Title){
+      const ToDo = {
+         Description: Description,
+         Title: Title,
+      }
+      localStorage.setItem("NewToDo", JSON.stringify(ToDo));
+      console.log(ToDo)
+   }
+
+   const CreateNewToDo = () => createNewToDo(Description, Title);
+
+
    return (
       <Container style={{background:"#760E63", maxWidth:"1280px", height:"252px", paddingBlock:"20px"}}>
          <Row>
             <Col md={6}>
                <Form.Group>
                   <Form.Label>Title</Form.Label>
-                  <Form.Control as="textarea" rows={1}/>
+                  <Form.Control as="textarea" rows={1} id="Title" onChange={getTitle}/>
                </Form.Group>
             </Col>
          </Row>
@@ -16,11 +40,11 @@ function CreateNewToDo(){
             <Col md={8}>
                <Form.Group>
                   <Form.Label>Description</Form.Label>
-                  <Form.Control as="textarea" rows={3} />
+                  <Form.Control as="textarea" rows={3} id="Description" onChange={getDescription}/>
                </Form.Group>     
             </Col>
          </Row>
-         <Button >Create</Button>
+         <Button onClick={CreateNewToDo}>Create</Button>
       </Container>
    )
 }
